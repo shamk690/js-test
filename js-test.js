8781f7b437dad952d0e6020081a5bf84a97cfcdb
Carl Evans <carlevans71969@gmail.com>
2020-02-07 21:45:02 +0000

    initial commit

---
+++ js-test.js
@@ -0,0 +1,13 @@
+// Task: generate 50 copies of an object with the following shape
+//
+// The `name` can be any string at all
+// The `group` should be either "x", "y" or "z" and each of those letters must appear at least once in the generated objects
+// The `id` must be a number which is unique amongst all the generated objects
+//
+// {
+//   name: string,
+//   group: string,
+//   id: number
+// }
+//
+
